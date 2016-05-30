718739b78d80904940dcaa308b7c05f6dfe6f884
Juraj <juraj.simon@bistudio.com>
2016-05-30 10:39:45 +0200

    create logs dir asynchronously

--- tests/unit/unit.js
+++ tests/unit/unit.js
@@ -1,9 +1,10 @@
 var assert = require('chai').assert;
 var fs = require('fs');
 var path = require('path');
+var process = require('process');
 
 //defined as env. var LOGS_DIR in tests.sh
-var logsDir = process.cwd() + '/tests/logs';
+var logsDir = process.cwd() + '/logs';
 
 suite('Routes', function () {
 
@@ -24,23 +25,41 @@ suite('Routes', function () {
     });
 
     test('create logs dir if it does not exists', function(done){
-        assert.isFalse(fs.existsSync(logsDir), 'Logs dir ' + logsDir + ' should not exists!');
-        var fileLog = require('../../lib/logger').getLoggerInstance();
-        assert.isTrue(fs.existsSync(logsDir), 'Logs dir ' + logsDir + ' should exists!');
-        done();
+        fs.stat(logsDir, function(err, stats) {
+            if (stats !== undefined) {
+                return done(new Error('Logs dir ' + logsDir + ' should not exists!'))
+            }
+
+            var filleLog = require('../../lib/logger').getLoggerInstance();
+            setTimeout(function() {
+                fs.stat(logsDir, function(err, stats) {
+                    if (err !== null) {
+                        return done(err)
+                    }
+                    assert.isTrue(stats.isDirectory(), 'Logs dir ' + logsDir + ' does not exists!');
+                    done();
+                })
+            }, 100)
+        })
     });
 
     test('return proper transport', function(done){
+        var isWin = /^win/.test(process.platform);
+        
         var fileLog = require('../../lib/logger');
         var transport = fileLog._getFileTransport('error', 'pretest_', 'tst');
         assert.equal(transport.level, 'error');
         assert.equal(transport.filename, 'pretest_');
         assert.equal(transport._basename, 'pretest_');
-        assert.equal(transport.dirname, logsDir.replace(/\\/g, '/'));
+        if (isWin) {
+            assert.equal(transport.dirname, logsDir.replace(/\//g, '\\'));
+        } else {
+            assert.equal(transport.dirname, logsDir.replace(/\\/g, '/'));
+        }
         assert.equal(transport.datePattern, '.yy-MM-dd.tst');
         done();
     });
-
+    
     test('do log does not produce error', function(done){
         var FileLog = require('../../lib/logger');
         FileLog.getLoggerInstance().err('TEST1');
